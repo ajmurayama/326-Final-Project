c5cc94bc561bbceeafa1b1acfdc08382e908b07f
gemjetsfan <jakestark99@gmail.com>
2021-04-12 17:44:01 -0400

    partial

--- contactbook.py
+++ contactbook.py
@@ -2,26 +2,23 @@
 ###Jake Stark
 
 class contact():
-""" A class representing a contact that will exist within the contact book
-
-Attributes:
-    pic: An image of the person
-    first_name(str): The first name of the person on in the contact book
-    last_name(str): The last name of the person
-    title(str): Title of the person
-    email(str): Contact's email
-    phone(str): Contact's phone number
-    
-Methods:
-    edit_contact: Allows user to edit details of a contact
-    equals: Allows for comparison of contact objects
-    
-
-
-
-"""
+    """ A class representing a contact that will exist within the contact book
+        
+        Attributes:
+            pic: An image of the person
+            first_name(str): The first name of the person on in the contact book
+            last_name(str): The last name of the person
+            title(str): Title of the person
+            email(str): Contact's email
+            phone(str): Contact's phone number
+        
+        Methods:
+            edit_contact: Allows user to edit details of a contact
+            equals: Allows for comparison of contact objects
+        
+    """
     pass
-    def __init__(self, pic, first_name, last_name, title, email, phone)
+    def __init__(self, pic, first_name, last_name, title, email, phone):
         self.pic = pic
         self.first_name = first_name
         self.last_name = last_name
@@ -30,25 +27,25 @@ Methods:
         self.phone = phone
     
     def equals(self, other):
-    """ A method that will compare two contacts and return true if they are the same and false otherwise
+        """ A method that will compare two contacts and return true if they are the same and false otherwise
 
-        Params:
-            other: contact that is being compared
-        
-        Returns:
-            A boolean that returns true if the contacts are the same.
-    """
-    if self.first_name != other.first_name:
-        return False
-    if self.last_name != other.last_name:
-        return False
-    if self.title != other.title:
-        return False
-    if self.email != other.email:
-        return False
-    if self.phone != other.phone:
-        return False
-    return True
+            Params:
+                other: contact that is being compared
+            
+            Returns:
+                A boolean that returns true if the contacts are the same.
+        """
+        if self.first_name != other.first_name:
+            return False
+        if self.last_name != other.last_name:
+            return False
+        if self.title != other.title:
+            return False
+        if self.email != other.email:
+            return False
+        if self.phone != other.phone:
+            return False
+        return True
     
     
     
--- test_contactbook.py
+++ test_contactbook.py
@@ -2,5 +2,16 @@ import pytest
 import contactbook as cbook
 
 def test_contactinit():
-    jake = contact("placeholder for image", "Jake","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
+    jake = cbook.contact("placeholder for image", "Jake","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
     assert jake.first_name == "Jake"
+    assert jake.last_name == "Stark"
+    assert jake.title == "Group Member"
+    assert jake.email == "Jakestark99@gmail.com"
+    assert jake.phone == "(203) 913-5328"
+    
+def test_goodequals():
+    john = ("placeholder for image", "John","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
+    jake = cbook.contact("placeholder for image", "Jake","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
+    assert jake.equals(john) == False
+    
+
