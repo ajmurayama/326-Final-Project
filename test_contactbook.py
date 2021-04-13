3024cf1b7806228f3747d0f0db179a25b14dd96d
gemjetsfan <jakestark99@gmail.com>
2021-04-12 20:19:26 -0400

    checkup

--- test_contactbook.py
+++ test_contactbook.py
@@ -9,9 +9,14 @@ def test_contactinit():
     assert jake.email == "Jakestark99@gmail.com"
     assert jake.phone == "(203) 913-5328"
     
-def test_goodequals():
-    john = ("placeholder for image", "John","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
+def test_falseequals():
+    john = cbook.contact("placeholder for image", "John","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
     jake = cbook.contact("placeholder for image", "Jake","Stark", "Group Member", "Jakestark99@gmail.com", "(203) 913-5328")
     assert jake.equals(john) == False
+    
+def test_trueequals():
+    sam = cbook.contact("placeholder for image", "Sam","Gold", "Friend", "samg@gmail.com", "(203) 933-5558")
+    sam_two = cbook.contact("placeholder for image", "Sam","Gold", "Friend", "samg@gmail.com", "(203) 933-5558")
+    assert sam.equals(sam_two) == True
     
 
