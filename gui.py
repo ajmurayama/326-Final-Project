6988fad92c25c7817a3e7541436d9391828f988d
tmbumd <82411110+tmbumd@users.noreply.github.com>
2021-05-14 17:55:44 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,21 +1,25 @@
 import sqlite3
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk,Image
 #from foo import Foo
-root = Tk()
+#class Gui():
+""" This class creates a graphical user interface with a display that takes in user input
+relating to contact information and then stores that into a database file. The user inut dissapears after the user 
+clicks the submit button"""
+root = tk.Tk()
 root.title("Contact Book")
-root.iconbitmap("c:/gui/finalproject.ico")
+	#root.iconbitmap("gui/finalproject.ico")
 root.geometry("500x500")
+conn = sqlite3.connect("contactbook.db")
+sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS contacts (
+                                        f_name text
+                                        l_name text
+                                        title  text
+                                        email  text
+                                        phonenumber integer
 
-
-
-
-
-submit_button = Button(root, text="Add Contact to Database", command=submit)
-submit_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
-query_button = Button(root, text="show contacts", command=query)
-query_button.grid()
-
+                                    ); """
+conn.commit()
 def submit():
 
 	conn = sqlite3.connect("contactbook.db")
@@ -24,13 +28,13 @@ def submit():
 
 	cursor.execute("INSERT INTO contacts VALUES (:f_name, :l_name, :title, :email, :phonenumber)",
 
-		{
-			'f_name': f_name.get(),
-			'l_name': l_name.get(),
-			'title': title.get(),
-			'email': email.get(),
-			'phonenumber': phonenumber.get()
-		})
+	{
+		'f_name': f_name.get(),
+		'l_name': l_name.get(),
+		'title': title.get(),
+		'email': email.get(),
+		'phonenumber': phonenumber.get()
+	})
 
 
 	conn.commit()
@@ -53,31 +57,50 @@ def query():
 	records = cursor.fetchall()
 	print(records)
 
-	for record in records[0]:
-		print_records += record + "\n"
+	for record in records:
+		print_records += str(record) + "\n"
 
-	conn.commit()
+		query_label = tk.Label(root, text=print_records)
+		query_label.grid(row=8, column=0, columnspan=2)
+		query_label.pack()
+		conn.commit()
 
 	conn.close()
-	
-
-f_name = Entry(root, width=30)
+		
+#creating and editing labels for buttons
+f_name = tk.Entry(root, width=30)
 f_name.grid(row=0, column=1, padx=20)
-l_name = Entry(root, width=30)
+l_name = tk.Entry(root, width=30)
 l_name.grid(row=1, column=1, padx=20)
-title = Entry(root, width=30)
+title = tk.Entry(root, width=30)
 title.grid(row=2, column=1, padx=20)
-email = Entry(root, width=30)
+email = tk.Entry(root, width=30)
 email.grid(row=3, column=1, padx=20)
-phonenumber = Entry(root, width=30)
+phonenumber = tk.Entry(root, width=30)
 phonenumber.grid(row=4, column=1, padx=20)
-f_name_label = Label(root, text="First Name")
+f_name_label = tk.Label(root, text="First Name")
 f_name_label.grid(row=0, column=0)
-l_name_label = Label(root, text="Last Name")
+l_name_label = tk.Label(root, text="Last Name")
 l_name_label.grid(row=1, column=0)
-title_label = Label(root, text="Title")
+title_label = tk.Label(root, text="Title")
 title_label.grid(row=2, column=0)
-email_label = Label(root, text="Email")
+email_label = tk.Label(root, text="Email")
 email_label.grid(row=3, column=0)
-phonenumber_label = Label(root, text="Phonenumber")
+phonenumber_label = tk.Label(root, text="Phonenumber")
 phonenumber_label.grid(row=4, column=0)
+
+#create buttons on interface, submit
+submit_button = tk.Button(root, text="Add Contact to Database", command=submit)
+submit_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+#create buttons on interface, query
+query_button = tk.Button(root, text="show contacts", command=query)
+query_button.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+
+
+#Commit
+conn.commit()
+
+#Close connection
+conn.close()
+
+root.mainloop()
