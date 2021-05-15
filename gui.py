4d5b021046cc451a9182d27c530c1836b5ec1c42
tmbumd <82411110+tmbumd@users.noreply.github.com>
2021-05-14 21:25:53 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,8 +1,11 @@
+#Tim, GUI
 import sqlite3
 import tkinter as tk
 from PIL import ImageTk,Image
 import os.path
-
+""" This python file contains a script that creates a database in your present working directory(the one you save this file to/import to)
+called "contactsbook.db"(assuming one does not already exist) Using tkinter, a gui interface was created that displays text box widgets which allow
+a user to input contact information. The buttons on the GUI application store contact information and display contact database results """
 root = tk.Tk()
 root.title("Contact Book")
 root.geometry("500x500")
@@ -11,7 +14,7 @@ def submit():
 	conn = sqlite3.connect("contactsbook.db")
 	c = conn.cursor()
 
-	c.execute(""" CREATE TABLE contacts (
+	c.execute(""" CREATE TABLE IF NOT EXISTS contacts (
                                         f_name text,
                                         l_name text,
                                         title text,
@@ -35,7 +38,7 @@ def submit():
 	conn.close()
 
 	#clear text boxes
-	f_name.delete(0, END)
+	f_name.delete(0, )
 	l_name.delete(0, END)
 	title.delete(0, END)
 	email.delete(0, END)
@@ -47,12 +50,11 @@ def query():
 
 	c.execute("SELECT * FROM contacts")
 
-	records = cursor.fetchall()
+	records = c.fetchall()
 	print(records)
 
 	for record in records:
 		print_records += str(record) + "\n"
-
 		query_label = tk.Label(root, text=print_records)
 		query_label.grid(row=8, column=0, columnspan=2)
 		query_label.pack()
@@ -96,4 +98,3 @@ query_button.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
 #conn.close()
 
 root.mainloop()
-
